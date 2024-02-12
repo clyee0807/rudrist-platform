e41ce21e287cde466634a649f4cbab862d31438c
leefuuchang <a0962014248gg@gmail.com>
2024-02-13 01:00:36 +0800

    Fix typo

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -27,7 +27,7 @@ const currencyDropDownItems = ref([
         <h2 class="title text-black-100 dark:text-white-100 text-4xl font-normal">Dashboard</h2>
         <div class="flex items-center justify-end flex-1 h-full gap-4 px-8">
             <div class="text-center text-2xl font-normal text-black-100 dark:text-white-100">
-                <DropDown :width="'10rem'" :height="'3rem'" :items="currencyDropDownItems"/>
+                <DropDown width="10rem" height="3rem" :items="currencyDropDownItems"/>
             </div>
             <div class="w-12 h-12">
                 <PromptButton icon="/_nuxt/assets/img/application/Profile.png" :active="false"/>
